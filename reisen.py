3a3fa001a801ca37733e1468dad4f1a81c4e3f5b
nicorevermann <contact@nicorevermann.com>
2024-08-26 23:16:34 +0200

    cleanup 2

--- reisen.py
+++ reisen.py
@@ -3,64 +3,73 @@
 import os
 import tomllib
 
-def get_conf(conf_file, keys):
+def get_conf(conf_file: str, keys: list) -> dict:
     with open(conf_file, "rb") as fh: conf = tomllib.load(fh)
     return {k: conf[k] if k in conf else "" for k in keys}
 
 class GalleryGenerator:
-    def __init__(self, img_path, filetree, template, output_path):
-        self.img_path = img_path
-        self.filetree = filetree
-        self.template = template
+    def __init__(self, base_path, file_tree, output_template, output_path):
+        self.base_path = base_path
+        self.output_template = output_template
         self.output_path = output_path
+        self.all_files = self.flatten_files(file_tree, (".jpg", ".jpeg", ".png", ".toml", ".gpx"))
+        self.images = self.flatten_files(file_tree, (".jpg", ".jpeg", ".png"))
+        self.gpx_data = self.flatten_files(file_tree, ".gpx")
+
+    def flatten_files(self, file_tree, file_type, current_path="") -> dict[str, list]:
+        files = {}
+        for key, value in file_tree.items():
+            if isinstance(value, dict):
+                new_path = f"{current_path}/{key}" if current_path else key
+                files.update(self.flatten_files(value, file_type, new_path))
+            elif key == "files":
+                filtered_files = [f for f in value if f.endswith(file_type)]
+                if filtered_files: files[current_path] = filtered_files
+        return files
 
     def generate_content(self):
-        for year in sorted(self.filetree, reverse=True):
-            for destination in sorted(self.filetree[year]):
-                content = ""
-                parameters = get_conf(f"{img_path}/{year}/{destination}/meta.toml", ["title"]) 
+        output = {}
+        for title,content in sorted(self.all_files.items()):
+            year_destination = str("/".join(title.split("/")[:2]))
+            if not year_destination in output: output[year_destination] = []
+            if year_destination == title: 
+                meta = get_conf(f"{self.base_path}{title}/meta.toml", ["title"])
+                output[year_destination].append(self.heading(year_destination.replace("/", "_"), meta["title"]))
+                if title in self.gpx_data: output[year_destination].insert(0, self.map())
+                if title in self.images: output[year_destination].append(self.gallery(year_destination.replace("/", "_") ,title))
+                output[year_destination].append('</div>\n</div>\n<br><hr>\n\n')
+            else: output[year_destination].append(self.chapter(year_destination.replace("/", "_"), title))
 
-                content += self.heading(f"{year}_{destination}", parameters["title"])
-                content += self.chapters(self.filetree[year][destination], f"{year}/{destination}")
-                content += '<div class="images">'
-                content += self.images([file for file in filetree[year][destination]["files"] if file.endswith((".jpg", ".jpeg", ".png"))], f"{year}/{destination}", f"{year}_{destination}")
-                content += "</div>"
+        for trip,content in sorted(output.items(), reverse=True):
+            self.out(f"{trip.replace("/", "_")}.html", "".join(content)) 
 
-
-                content += '</div>\n</div>\n<br><hr>\n\n'
-                self.out(self.output_path, f"{year}_{destination}.html", "{content}", content)
-
-    def heading(self, html_class, title):
+    def heading(self, html_class: str, title: str) -> str:
         return f'<div id="{html_class}">\n<h2>{title}</h2>\n<div>\n'
 
-    def images(self, images, path: str, html_class: str):
-        if images == []: return ""
+    def gallery(self, html_class, key: str) -> str:
+        output = ['<div class="images">']
+        for image in sorted(self.images[key]):
+            img_path = self.base_path + key + "/" + image
+            img_title = image.split("_")[1].split(".")[0] if image.split("_")[1] else ""
+            output.append(f'<a href="{img_path}" data-lightbox="{html_class}" data-title="{img_title}" data-alt="{img_title}"><img src="{img_path}" loading="lazy"></a>\n')
+        return "".join(output) + "</div>"
 
-        images_content = ""
-        for images in sorted(images):
-            img_path = f"{self.img_path}{path}/{images}"
-            img_title = images.split(".")[0].split("_")[1]
-            images_content += f'<a href="{img_path}" data-lightbox="{html_class}" data-title="{img_title}" data-alt="{img_title}"><img src="{img_path}" loading="lazy"></a>\n'
-        return images_content
+    def map(self):
+        pass
 
-    def chapters(self, chapters, path: str):
-        chapter_content = ""
-        for chapter in sorted(chapters):
-            if "files" not in chapter:
-                parameters = get_conf(f"{self.img_path}/{path}/{chapter}/meta.toml", ["title"])
-                chapter_content += f'<div class="chapter">\n<h4>{parameters["title"]}</h4>\n<div class="images">\n' if parameters["title"] != "" else f'<div class="chapter">\n<div class="images">\n'
-                chapter_content += self.images([file for file in chapters[chapter]["files"] if file.endswith(("jpg", "jpeg", "png"))], f"{path}/{chapter}", path.replace("/", "_"))
-                chapter_content += '</div>\n</div>\n<br><center>---<center><br>\n\n'
-        
-        return chapter_content
+    def chapter(self, html_class: str, key: str) -> str:
+        output = ["<div>"]
+        meta = get_conf(f"{self.base_path}{key}/meta.toml", ["title"])
+        if "title" in meta: output.append(self.heading("_".join(key.split("/")[:2]), meta["title"]))
+        if key in self.images: output.append(self.gallery(html_class, key))
+        return "".join(output) + "</div><br>"
 
-    def out(self, output_path, output_name, replacement_expression, output):
-        with open(self.template, "r") as fh: page = fh.read()
-        with open(output_path + output_name, "w") as fh: fh.write(page.replace(replacement_expression, output))
+    def out(self, output_name, output_html):
+        with open(self.output_template, "r") as fh: page = fh.read()
+        with open(self.output_path + output_name, "w") as fh: fh.write(page.replace("{content}", output_html))
 
 def generate_links(img_path, filetree, template, output_path):
     links = ""
-
     for year in sorted(filetree, reverse=True):
         for destination in filetree[year]:
             links += '<div class="links">'
@@ -68,13 +77,11 @@ def generate_links(img_path, filetree, template, output_path):
             with open(path + "/meta.toml", "rb") as fh: meta = tomllib.load(fh)
             links += f'<a href="{year}_{destination}.html">{meta['title']}</a>\n'
             links += '</div>'
-
     with open(template, "r") as fh: page = fh.read()
     with open(output_path + "index.html", "w") as fh: fh.write(page.replace("{links}", links))
 
 def generate_filetree(img_path):
     tree = {}
-
     for entry in os.listdir(img_path):
         full_path = os.path.join(img_path, entry)
         if os.path.isdir(full_path): tree[entry] = generate_filetree(full_path)
@@ -85,12 +92,10 @@ def generate_filetree(img_path):
 
 if __name__ == "__main__":
     parameters = get_conf("conf/conf.toml", ["img_path", "template_links", "template_trip", "output_path"])
-
     img_path = parameters["img_path"]
     template_links = parameters["template_links"]
     template_trip = parameters["template_trip"]
     output_path = parameters["output_path"]
-
     filetree = generate_filetree(img_path)
 
     generate_links(img_path, filetree, template_links, output_path)
